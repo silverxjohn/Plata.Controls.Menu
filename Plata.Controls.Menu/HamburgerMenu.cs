5dc733c2c383aa3795ce278ee184dfe63c941004
John Aldrin Plata <king.aldrin31@gmail.com>
2016-05-23 16:26:31 +0800

    FIrst working version

---
+++ Plata.Controls.Menu/HamburgerMenu.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Plata.Controls.Menu
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:Plata.Controls.Menu"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:Plata.Controls.Menu;assembly=Plata.Controls.Menu"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:HamburgerMenu/>
+    ///
+    /// </summary>
+    public class HamburgerMenu : ContentControl
+    {
+        public new List<HamburgerMenuItem> Content
+        {
+            get { return (List<HamburgerMenuItem>)GetValue(ContentProperty); }
+            set { SetValue(ContentProperty, value); }
+        }
+
+        public new static readonly DependencyProperty ContentProperty =
+            DependencyProperty.Register("Content", typeof(List<HamburgerMenuItem>), typeof(HamburgerMenu),
+            new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        static HamburgerMenu()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(HamburgerMenu), new FrameworkPropertyMetadata(typeof(HamburgerMenu)));
+        }
+
+        public override void BeginInit()
+        {
+            Content = new List<HamburgerMenuItem>();
+            base.BeginInit();
+        }
+
+        public bool IsOpen
+        {
+            get { return (bool)GetValue(IsOpenProperty); }
+            set
+            {
+                SetValue(IsOpenProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty IsOpenProperty =
+            DependencyProperty.Register("IsOpen", typeof(bool), typeof(HamburgerMenu), new PropertyMetadata(true));
+
+
+        public System.Windows.Media.Brush MenuIconColor
+        {
+            get { return (System.Windows.Media.Brush)GetValue(MenuIconColorProperty); }
+            set { SetValue(MenuIconColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty MenuIconColorProperty =
+            DependencyProperty.Register("MenuIconColor", typeof(System.Windows.Media.Brush), typeof(HamburgerMenu), new PropertyMetadata(System.Windows.Media.Brushes.White));
+
+
+        public Brush SelectionIndicatorColor
+        {
+            get { return (Brush)GetValue(SelectionIndicatorColorProperty); }
+            set { SetValue(SelectionIndicatorColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectionIndicatorColorProperty =
+            DependencyProperty.Register("SelectionIndicatorColor", typeof(Brush), typeof(HamburgerMenu), new PropertyMetadata(Brushes.Red));
+
+        public Brush MenuItemForeground
+        {
+            get { return (Brush)GetValue(MenuItemForegroundProperty); }
+            set { SetValue(MenuItemForegroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty MenuItemForegroundProperty =
+            DependencyProperty.Register("MenuItemForeground", typeof(Brush), typeof(HamburgerMenu), new PropertyMetadata(Brushes.Transparent));
+    }
+}
---
+++ Plata.Controls.Menu/HamburgerMenuItem.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+namespace Plata.Controls.Menu
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:Plata.Controls.Menu"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:Plata.Controls.Menu;assembly=Plata.Controls.Menu"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:HamburgerMenuItem/>
+    ///
+    /// </summary>
+    public class HamburgerMenuItem : ListBoxItem
+    {
+        static HamburgerMenuItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(HamburgerMenuItem), new FrameworkPropertyMetadata(typeof(HamburgerMenuItem)));
+        }
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(HamburgerMenuItem), new PropertyMetadata(String.Empty));
+
+        public string Icon
+        {
+            get { return (string)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(string), typeof(HamburgerMenuItem), new PropertyMetadata(""));
+
+        public Brush SelectionIndicatorColor
+        {
+            get { return (Brush)GetValue(SelectionIndicatorColorProperty); }
+            set { SetValue(SelectionIndicatorColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectionIndicatorColorProperty =
+            DependencyProperty.Register("SelectionIndicatorColor", typeof(Brush), typeof(HamburgerMenuItem), new PropertyMetadata(Brushes.Blue));
+
+        public ICommand SelectionCommand
+        {
+            get { return (ICommand)GetValue(SelectionCommandProperty); }
+            set { SetValue(SelectionCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectionCommandProperty =
+            DependencyProperty.Register("SelectionCommand", typeof(ICommand), typeof(HamburgerMenuItem), new PropertyMetadata(null));
+    }
+}
